ff47651b851ae75456e540324d15e0e1a8bfd406
oliveiramiro2 <ramiromares95@gmail.com>
2021-11-09 02:01:16 -0300

    add pesquisa individual de poke por parametros http

--- src/router/pokemon.js
+++ src/router/pokemon.js
@@ -3,8 +3,9 @@ const buscar = require('../pokeApi/index')
 
 const router = express.Router()
 
-router.get('/pokemon:num?', (req, res) => {
-    console.log("ok", req.params())
+router.get('/pokemon', async (req, res) => {
+    const data = await buscar("/pokemon/"+String(req.query.num))
+    res.send(data.name)
 })
 
 module.exports = app => app.use("/info", router)
