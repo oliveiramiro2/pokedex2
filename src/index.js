34d5c981f2bd5632b68829c62cb12db42a8354ee
oliveiramiro2 <ramiromares95@gmail.com>
2021-11-09 01:17:36 -0300

    começando o projeto

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const baseUrl = require('./pokeApi/index')
+
+const axios = require('axios')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended:false }))
+
+app.get("/", (req, res) => {
+    res.send("tudo ok")
+})
+
+app.get("/pokemons", (req, res) => {
+    axios.get(baseUrl+"pokemon").then((response) => {
+        const data = response.data
+        res.json(data)
+    }).catch(error => {
+        res.send({ Erro: 'Não foi possivel concluir a requisição' })
+    })
+})
 
 const port = 3001
 app.listen(port, ()=>console.log('Rodando servidor'))
---
+++ src/pokeApi/index.js
@@ -0,0 +1,3 @@
+const baseUrl = 'https://pokeapi.co/api/v2/'
+
+module.exports = baseUrl
